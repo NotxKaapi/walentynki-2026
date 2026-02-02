1e17801be61118b86f23d9946b5ae505b1cc8ef5
NotxKaapi <105313344+NotxKaapi@users.noreply.github.com>
2026-02-02 12:47:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jula",
+    valentineName: "Julcia, Kochanie",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
